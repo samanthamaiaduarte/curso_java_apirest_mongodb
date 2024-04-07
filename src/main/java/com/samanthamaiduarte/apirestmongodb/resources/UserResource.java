5d6ee2636104bb5bc65413a678fb336ba6864921
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-07 16:24:09 -0300

    Inserção de usuário com POST

--- src/main/java/com/samanthamaiduarte/apirestmongodb/resources/UserResource.java
+++ src/main/java/com/samanthamaiduarte/apirestmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.samanthamaiduarte.apirestmongodb.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.samanthamaiduarte.apirestmongodb.domain.User;
 import com.samanthamaiduarte.apirestmongodb.dto.UserDTO;
@@ -32,5 +36,13 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		UserDTO userDTO = new UserDTO(service.findById(id));
 		return ResponseEntity.ok().body(userDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO userDTO) {
+		User user = service.insert(service.fromDTO(userDTO));
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/samanthamaiduarte/apirestmongodb/services/UserService.java
+++ src/main/java/com/samanthamaiduarte/apirestmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.samanthamaiduarte.apirestmongodb.domain.User;
+import com.samanthamaiduarte.apirestmongodb.dto.UserDTO;
 import com.samanthamaiduarte.apirestmongodb.repository.UserRepository;
 import com.samanthamaiduarte.apirestmongodb.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> user = repository.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
