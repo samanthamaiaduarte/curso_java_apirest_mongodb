3aebfe8d49979980ce336661b8278a43a10fc36f
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-07 10:07:57 -0300

    Project created

---
+++ src/main/java/com/samanthamaiduarte/apirestmongodb/ApirestMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.samanthamaiduarte.apirestmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samanthamaiduarte/apirestmongodb/ApirestMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samanthamaiduarte.apirestmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
