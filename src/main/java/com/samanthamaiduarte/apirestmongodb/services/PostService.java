5df863d2c9063381ddf07eff27b87ea5c1971c99
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-08 15:48:11 -0300

    Endpoint para obter um post por id

---
+++ src/main/java/com/samanthamaiduarte/apirestmongodb/services/PostService.java
@@ -0,0 +1,24 @@
+package com.samanthamaiduarte.apirestmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.samanthamaiduarte.apirestmongodb.domain.Post;
+import com.samanthamaiduarte.apirestmongodb.repository.PostRepository;
+import com.samanthamaiduarte.apirestmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found! Id: " + id));
+	}
+	
+
+}
