9efce5457d541c1fb7c40e0de5408d04b94f7df1
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-04-08 15:37:36 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/samanthamaiduarte/apirestmongodb/resources/UserResource.java
+++ src/main/java/com/samanthamaiduarte/apirestmongodb/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.samanthamaiduarte.apirestmongodb.domain.Post;
 import com.samanthamaiduarte.apirestmongodb.domain.User;
 import com.samanthamaiduarte.apirestmongodb.dto.UserDTO;
 import com.samanthamaiduarte.apirestmongodb.services.UserService;
@@ -62,4 +63,11 @@ public class UserResource {
 		
 		return ResponseEntity.noContent().build();
 	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user.getPosts());
+	}
+	
 }
